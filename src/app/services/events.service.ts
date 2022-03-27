aa6b55fd69b8968d6877b8df9525f12d52ef82da
ayoubc <ayoubch807@gmail.com>
2022-03-27 20:31:39 +0000

    implement base http client class

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable, BehaviorSubject } from 'rxjs';
+import {BaseHttpClient} from '../infrastructure/http-clients/base.http-client';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventsService {
+export class EventsService extends BaseHttpClient {
+  
   private BASE_URI: string = "";
-  private EVENT_LIST_SEGMENT: string = "";
-  private CREATE_EVENT_SEGMENT: string = "";
+  private EVENT_LIST_ENDPOINT: string = "";
+  private CREATE_EVENT_ENDPOINT: string = "";
 
-  constructor(private httpClient: HttpClient) { }
-
-  getEvents() {
-    return this.httpClient.get(`${this.BASE_URI}\\${this.EVENT_LIST_SEGMENT}`);
+  constructor(private httpClient: HttpClient) { 
+    super(httpClient);
   }
 
-  createEvent(event: any) {
-    return this.httpClient.post(`${this.BASE_URI}\\${this.CREATE_EVENT_SEGMENT}`, event);
+  protected getBaseUrl(): string {
+      return this.BASE_URI;
+  }
+
+  getEvents(): Observable<any> {
+    return this.get(this.EVENT_LIST_ENDPOINT);
+  }
+
+  createEvent(event: any): Observable<any> {
+    return this.post(this.CREATE_EVENT_ENDPOINT, event);
   }
 
 }
